1c284e39674460aa2b1a0ee4b5caa290260dc885
revalinda <revalindaph@gmail.com>
2025-12-01 19:52:09 +0700

    Ralat Print Bil. Genap 1-20

--- Looping1/Level1.java
+++ Looping1/Level1.java
@@ -1,12 +1,12 @@
 public class Level1 {
 
     public static void main(String[] args) {
-        int i = 0;
-        while (i < 20) {
-            if (i % 2 == 0) { // i dibagi 2 sisanya 0
-                System.out.print(i+1); // print i sesuai looping ditambah 1 karena i=0
+        int i = 1;
+        while (i < 21) {
+            if (i %2 == 0) { // i dibagi 2 sisanya 0
+                System.out.print(i); // print i sesuai looping
             }
-            i++; // mengulang i=0 i=1 sampai batas yg ditentukan (i<20)
+            i++; // mengulang i=1, i=2 dst. sampai batas yg ditentukan (i<20)
         }
     }
 }
