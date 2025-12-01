a762e30d61df5d7a26f2693ca8276c6f9bc0a5d4
revalinda <revalindaph@gmail.com>
2025-12-01 20:13:31 +0700

    Print Bil.Genap (For) 10-1

--- Looping1/Level1.java
+++ Looping1/Level1.java
@@ -1,12 +1,14 @@
 public class Level1 {
 
     public static void main(String[] args) {
-        int i = 1;
-        while (i < 21) {
-            if (i %2 == 0) { // i dibagi 2 sisanya 0
-                System.out.print(i); // print i sesuai looping
+        int i = 10;
+        while (i >= 1) {
+                System.out.print(i+" "); 
+            i--; 
             }
-            i++; // mengulang i=1, i=2 dst. sampai batas yg ditentukan (i<20)
+
+        for (int j = 10; j >= 1; j--) {
+            System.out.print(j+" ");
         }
     }
 }
