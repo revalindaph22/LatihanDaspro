5f1eca1ff35c68a830822bb57aafa62874488612
revalinda <revalindaph@gmail.com>
2025-12-01 19:49:06 +0700

    Print Bilangan Genap 1-20

--- Looping1/Level1.java
+++ Looping1/Level1.java
@@ -1,8 +1,12 @@
 public class Level1 {
 
     public static void main(String[] args) {
-        for (int i = 0; i < 10; i++) {
-            System.out.print(i+1);
+        int i = 0;
+        while (i < 20) {
+            if (i % 2 == 0) { // i dibagi 2 sisanya 0
+                System.out.print(i+1); // print i sesuai looping ditambah 1 karena i=0
+            }
+            i++; // mengulang i=0 i=1 sampai batas yg ditentukan (i<20)
         }
     }
 }
